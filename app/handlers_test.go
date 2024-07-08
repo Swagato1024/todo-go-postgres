07a779b63998ef3a64429cc4ad95c98a9ab90e32
Swagato Chakraborty <swagatochakraborty@Swagatos-MacBook-Pro.local>
2024-07-08 14:32:51 +0530

    Add test for Internal server error

--- app/handlers_test.go
+++ app/handlers_test.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -12,7 +13,7 @@ import (
 	"github.com/todo/models"
 )
 
-func TestSendTodos(t *testing.T) {
+func TestSendTodos_Success(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	mockRepo := db.NewMockTodoRepository(ctrl)
@@ -25,8 +26,22 @@ func TestSendTodos(t *testing.T) {
 
 	app := CreateApp(mockRepo)
 	req := httptest.NewRequest(http.MethodGet, "/todos", nil)
-	resp, err := app.Test(req)
+	resp, errInReq := app.Test(req)
 
-	assert.NoError(t, err)
+	assert.NoError(t, errInReq)
 	assert.Equal(t, http.StatusOK, resp.StatusCode, "Expected status code 200")
+}
+
+func TestSendTodos_ServerError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockRepo := db.NewMockTodoRepository(ctrl)
+
+	mockRepo.EXPECT().GetAllTodo().Return(nil, errors.New("mock error"))
+
+	app := CreateApp(mockRepo)
+	req := httptest.NewRequest(http.MethodGet, "/todos", nil)
+	resp, _:= app.Test(req)
+
+	assert.Equal(t, http.StatusInternalServerError, resp.StatusCode, "Expected Internal server Error")
 }
